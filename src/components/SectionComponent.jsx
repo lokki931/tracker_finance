3717f6a56df2841b013932f75b288590ba6aa6d5
lokki931 <taurusr2d2@gmail.com>
2025-04-30 19:31:51 +0300

    shadow dark mode

--- src/components/SectionComponent.jsx
+++ src/components/SectionComponent.jsx
@@ -1,6 +1,6 @@
 export const SectionComponent = ({ title, children }) => {
   return (
-    <section className="p-4 shadow-lg mb-4">
+    <section className="p-4 shadow-lg dark:shadow-gray-500 mb-4">
       <h3 className="text-lg font-semibold mb-2">{title}</h3>
       {children}
     </section>
